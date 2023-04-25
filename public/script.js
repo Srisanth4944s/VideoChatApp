9404d833741c6fdfe311e381da898d23456b5ff8
Srisanth4944s <nisanithya83@gmail.com>
2023-04-25 18:18:46 +0530

    videoAdded

--- public/script.js
+++ public/script.js
@@ -5,6 +5,26 @@ var peer = new Peer(undefined,{
     port:"443"
 })
 const user = prompt("Enter Your Name")
+const myVideo = document.createElement("video")
+myVideo.muted = true
+
+let myStream;
+navigator.mediaDevices.getUserMedia({
+    audio:true,
+    video:true
+})
+.then((stream)=>{
+    myStream = stream
+    addVideoStream(myVideo,stream)
+})
+function addVideoStream(video,stream){
+    video.srcObject = stream
+    video.addEventListener("loadmetedata",()=>{
+        video.play()
+        $("#video_grid").append(video)
+    })
+}
+
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
